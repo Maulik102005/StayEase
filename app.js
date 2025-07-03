cdfe521c38157f544037e7244a3196a1dcbbff9e
Maulik Dave <maulikdave2005@gmail.com>
2025-07-03 16:10:27 +0530

    `Added CRUD routes for listings, updated dependencies and package files`

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+//ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//allows POST's use via req.body
+app.use(express.urlencoded({ extended: true }));
+//Post to put or post to delete
+app.use(methodOverride("_method"));
+
+//ejsMate
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
 
 main()
   .then(() => {
@@ -21,18 +36,51 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/testListing", async (req, res) => {
-  const sampleListing = new Listing({
-    title: "Test Apartment",
-    description: "A cozy place to stay.",
-    price: 1000,
-    location: "Mumbai",
-    country: "India",
-  });
-  await sampleListing.save();
-  res.send("Sample listing saved!");
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
 });
 
+//New Route
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Read Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect("/listings");
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
 app.listen(8080, () => {
   console.log("server is running");
 });
